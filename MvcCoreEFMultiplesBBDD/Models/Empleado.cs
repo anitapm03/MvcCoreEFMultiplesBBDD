2556d5d0ac63a517ff7fe7b834c00af9a959c36f
anitapm03 <apereiramolinero@gmail.com>
2024-02-21 12:47:01 +0100

    Add project files.

---
+++ MvcCoreEFMultiplesBBDD/Models/Empleado.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcCoreEFMultiplesBBDD.Models
+{
+    [Table("v_empleados")]
+    public class Empleado
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Column("APELLIDO")]
+        public string Apellido { get; set; }
+
+        [Column("OFICIO")]
+        public string Oficio { get; set;}
+
+        [Column("DEPARTAMENTO")]
+        public string Departamento { get; set; }
+
+        [Column("LOCALIDAD")]
+        public string Localidad { get; set; }
+
+        [Column("DEPT_NO")]
+        public int IdDepartamento { get; set; }
+    }
+}
---
+++ MvcCoreEFMultiplesBBDD/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEFMultiplesBBDD.Data;
+using MvcCoreEFMultiplesBBDD.Models;
+
+namespace MvcCoreEFMultiplesBBDD.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            var consulta = from datos in this.context.Empleados
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+        public Empleado FindEmpleado(int id)
+        {
+            var consulta = from datos in this.context.Empleados
+                           where datos.Id == id
+                           select datos;
+            return consulta.First();
+
+        }
+
+
+    }
+}
